fa51f23fcbd4dc031291712598f0778857bedfd3
SzathRobi <szathrobi98@gmail.com>
2022-02-17 12:19:34 +0100

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
+import Script from "next/script";
 import ThemeWrapper from "../contexts/ThemeWrapper";
 import "../styles/globals.css";
 
+/*<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0YQ9VC6L4N"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0YQ9VC6L4N');
+</script>*/
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeWrapper>
-      <Component {...pageProps} />
-    </ThemeWrapper>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-0YQ9VC6L4N"}
+      />
+      <Script id="testId" strategy="lazyOnload">
+        {" "}
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-0YQ9VC6L4N');`}
+      </Script>
+      <ThemeWrapper>
+        <Component {...pageProps} />
+      </ThemeWrapper>
+    </>
   );
 }
 
